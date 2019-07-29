0e19fa6ced67f8714421b2b9f263e42e80e8a4c6
Nipun Garg <nipun@nipungarg.me>
2019-07-29 06:28:41 +0530

    Implement Binary Tree Transform

--- main.cpp
+++ main.cpp
@@ -1,17 +1,42 @@
 #include "simple-grid.h"
 #include <opencv2/highgui.hpp>
 
+void binaryTreeTransform(AbstractGrid *grid) {
+	Cell *cell;
+	for (uint i = 0; i < grid->_rows; i++) {
+		for (uint j = 0; j < grid->_cols; j++) {
+			cell = grid->getCell(i, j);
+			std::cout << "Row: " << i << " Column: " << j << "\n";
+			if (cell->_north != nullptr && cell->_east != nullptr) {
+				uint chance = grid->rand(2);
+				std::cout << "Chance: " << chance << "\n";
+				if (chance)
+					cell->link(cell->_north);
+				else
+					cell->link(cell->_east);
+			} else if (cell->_east != nullptr)
+				cell->link(cell->_east);
+			else if (cell->_north != nullptr)
+				cell->link(cell->_north);
+		}
+	}
+	std::cout << "\n";
+}
+
 int main() {
-	SimpleGrid x(4, 4);
+	SimpleGrid x1(4, 4);
+	SimpleGrid x2(4, 4);
+	SimpleGrid x3(4, 4);
+	SimpleGrid x4(4, 4);
+	binaryTreeTransform(&x1);
+	binaryTreeTransform(&x2);
+	binaryTreeTransform(&x3);
+	binaryTreeTransform(&x4);
 
-	Cell *ptr = x.getCell(1, 1);
-
-	ptr->link(ptr->_north);
-
-	std::cout << x;
+	std::cout << x1;
 
 	cv::namedWindow("Display");
-	cv::imshow("Display", x.saveImage("output.png"));
+	cv::imshow("Display", x1.saveImage("output.png"));
 
 	cv::waitKey(0);
 
